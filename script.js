a9ff1b6ebf51ba10daeea5c4993d44ae51b9c417
Talos0248 <talos0248@gmail.com>
2024-03-11 08:57:53 +0800

    Implement basic CSS Grid layout

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
+const poopBtn = document.getElementById("btn-poop")
+const poopCtr = document.getElementById("ctr-poop")
 
 
-
-
+let poop = 0
+let poopPerClick = 1
+poopBtn.addEventListener("click",()=>{
+    poop += poopPerClick;
+    poopCtr.innerText = `Poop: ${poop}`
 })
+
+
